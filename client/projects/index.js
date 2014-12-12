377c770cb0464b5af218daa5c783de9b29b65c87
Vitaly Sukhomlinov <v-vsukhomlinov@hotwire.com>
2014-12-12 11:30:39 -0800

    fixing template insertions through Angular

--- client/projects/index.js
+++ client/projects/index.js
@@ -9,5 +9,12 @@ var app = angular.module('projects', ['alerts', 'moment', 'ui.bootstrap.buttons'
   .config(['$interpolateProvider', interpolate])
   .controller('ManualController', ['$scope', '$sce', ManualController])
   .controller('ProjectsController', ['$scope', ProjectsController])
+  .directive('pluginTemplate', ['$compile', function($compile) {
+    return {
+      link: function(scope, element, attr) {
+        element.append($compile('<div class="provider-specific">' + scope.$eval(attr['pluginTemplate']) + '</div>')(scope))
+      }
+    }
+  }]);
 
 module.exports = app; 
